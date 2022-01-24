3a4d2ebfc80cdc6b058bc13761555b561aca2514
Devan Cobb <devancobb.dev@gmail.com>
2022-01-24 16:09:00 -0700

    created ul-el for leads

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-let inputBtn = document.getElementById("input-btn")
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
 inputBtn.addEventListener("click", function () {
-    console.log("button clicked")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+}
